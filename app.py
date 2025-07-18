f94bffc41806fae4b89b441a6fc6b5238bd60a3b
Abed Nabil <A.abuhelwe@gmail.com>
2025-07-18 18:06:34 +0300

    add class to py file

--- app.py
+++ app.py
@@ -6,3 +6,16 @@ arr =[1, 2, 3, 4]
 
 def squared_num(x):
   return x * x
+
+class Animal{
+        def __init__(self,name,age):
+            self.name = name
+            self.age  = age 
+
+        def __str__(self):
+            return f'{self.name} is {self.age} years old'
+
+        def __len__(self):
+            return self.age 
+
+    }
